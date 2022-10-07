14aece8b3c07b64698ac2985c1fbb17a6fd91139
Joel Drapper <joel@drapper.me>
2022-10-07 21:31:14 +0100

    Period::Days#in_batches_of

--- lib/playdate/period.rb
+++ lib/playdate/period.rb
@@ -22,6 +22,17 @@ module Playdate
 			def size
 				(last.date - first.date).to_i + 1
 			end
+
+			def in_batches_of(n)
+				Enumerator.new do |yielder|
+					(size / n).ceil.times do |g|
+						from = first + (n * g)
+						to = from + n
+
+						yielder << self.class.new(from...to)
+					end
+				end
+			end
 		end
 
 		class Weeks < Period
